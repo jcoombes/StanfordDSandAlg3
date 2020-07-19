c6f01a3dac01de773b4deef00804f01d140ae297
jcoombes <coombes.jd@gmail.com>
2020-07-19 12:22:47 +0000

    Streamlined

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import math
-import statistics
-import logging
-import datetime
-import utils
+import math #used for math.floor
+import statistics #used to calculate median.
+import utils #I created a helper function to check if list sorted.
 
-logging.basicConfig(filename='example.log', level=logging.INFO)
-logging.info(datetime.datetime.now())
+def quick_sort(to_sort: list, left: int, right: int, pivot_choice: str) -> list:
+  """
+  Input:
+  to_sort - the list to be sorted. like sorted(), quick_sort sorts in place.
 
-def quick_sort(to_sort: list, left: int, right: int, method: str) -> list:
+  left, right - these are list indices, when we make recursive calls to this function, we don't pass in slices of to_sort, we pass in left and right indices.
+
+  pivot_choice - 'first', 'median', 'last' which pivot would you like? see choose pivot for further information.
+
+  returns to_sort - the sorted list.
+  """
     if left >= right:
-        logging.debug(f"left:{left}, right:{right}")
         return
 
-    p = choose_pivot(to_sort, left, right, method)
-
-    logging.debug(f"p: {p}, left: {left}, right:{right}")
-
-    to_sort[p], to_sort[left] = to_sort[left], to_sort[p]
-    new_piv = bucket(to_sort, left, right)  #mutates to_sort
+    p = choose_pivot(to_sort, left, right, pivot_choice)
+    #pre-process for bucket subroutine by swapping pivot and first element.
+    to_sort[p], to_sort[left] = to_sort[left], to_sort[p] 
+    #bucket() partitions the list into [ <p, p, >p ].
+    #Note that <p is not sorted. >p is also not sorted.
+    new_piv = bucket(to_sort, left, right)  
     quick_sort(to_sort, left, new_piv, method)
     quick_sort(to_sort, new_piv + 1, right, method)
     return to_sort
@@ -35,12 +39,21 @@ def choose_pivot(to_sort, left, right, method):
         return median_pivot(to_sort, left, right)
 
 def median_pivot(to_sort, left, right):
+  """
+  Median of three pivot rule takes the first, middle and last elements of the list.
+
+  For an odd length list, middle is defined as expected.
+  For an even length list, middle picks the left of the two middle elements.
+
+  Then chooses the median of those first, middle, last.
+  """
+  
   first = left
   mid = math.floor((left + right - 0.5) // 2)
   last = right - 1
 
+  #We need first, middle and last to be distinct.
   if right == left + 2 or right == left + 1:
-    logging.debug(f"f:{first}, m:{mid}, l:{last}, median:{left}")
     return left
 
   d = {
@@ -50,24 +63,20 @@ def median_pivot(to_sort, left, right):
   }
 
   med_val = int(statistics.median(d.keys()))
-  try:
-    median_idx = d[med_val]
-  except KeyError:
-    logging.warning(f"f:{first}, m:{mid}, l:{last}, median:{left}")
-    logging.warning(f"d:{d}, med_val: {med_val}")
-    logging.warning(to_sort[left:right])
-    raise KeyError(med_val) 
-  logging.debug(f"f:{first}, m:{mid}, l:{last}, median:{median_idx}")
+  median_idx = d[med_val]
   return median_idx
 
 
 def bucket(l: list, left: int, right: int) -> None:
-    """
+  """
   partitions a list l around a pivot index.
   Mutates the original list and does not return anything.
   A subroutine, not a function.
 
   left <= right. The left and right endpoints of the partition.
+  e.g. bucket([0, 1, 2, 5, 3, 4], 3, 5) would only partition 
+  [5, 3, 4]
+
   """
     pivot = left
     i = left + 1
@@ -82,13 +91,16 @@ def bucket(l: list, left: int, right: int) -> None:
 
 
 def qs_file(list_length: int):
-  
+  """
+  Open QuickSort.txt and extract the first list_length integers.
+
+  If list_length < 0, get the whole txt file of 10,000 ints.
+  """
   if list_length < 0:
     long_boi = []
     with open("QuickSort.txt") as f:
         for line in f.readlines():
             long_boi.append(int(line[:-1]))
-        logging.info(f"longboi is {len(long_boi)} long")
     return long_boi
 
   with open("QuickSort.txt") as f:
@@ -99,13 +111,9 @@ def main(to_sort: list, pivot_choice: str):
 
     bucket.comp = 0
     quick_sort(to_sort, 0, len(to_sort), pivot_choice)
-    logging.info(f"sported longboi is {len(to_sort)} long")
-    logging.info(f"There were {bucket.comp} comparisons")
-
     did_it_work = utils.is_sorted(to_sort)
 
     print(f"For input size {len(to_sort)}, we made {bucket.comp} comparisons. \nWe used the {pivot_choice} element as our pivot. \nIs our list sorted?: {did_it_work}")
-
 
     return bucket.comp, to_sort
 
